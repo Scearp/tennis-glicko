616acff5501b1fed5791e4c29b081418eeff32ad
Scearp <cefperry@gmail.com>
2022-08-17 22:23:38 -0700

    Automate insertion of underscores in surnames (oop.py)

--- bracket/oop.py
+++ bracket/oop.py
@@ -1,26 +1,37 @@
+from msilib.schema import Directory
 import pandas as pd
+
 import sys
 
 def first_cap(string):
-    return string[0].upper() + string[1:]
+    if len(string) > 1:
+        return string[0].upper() + string[1:]
+    return string[0].upper()
+
+def _format_name(name):
+    match_types = ['tb1', 'tb2', 'tb3', 'sr']
+
+    name = [t.lower() for t in name if t.lower() not in match_types]
+    return [first_cap(n) for n in name]
 
 def format_name(name):
-    name = name.lower()
     name = name.replace('-', ' ')
-    match_types = ['tb1', 'tb2', 'tb3', 'sr']
-    name_list = name.split(' ')
-    for type in match_types:
-        name_list = [n for n in name_list if n != type]
-    try:
-        name_list = [first_cap(n) for n in name_list]
-    except:
-        raise ValueError(name, name_list)
-    name = " ".join(name_list)
-    
-    return name
+    names = []
+    surnames = []
+
+    for token in name.split(' '):
+        if token.upper() == token:
+            surnames.append(token)
+        else:
+            names.append(token)
+
+    name = " ".join(_format_name(names))
+    surname = "_".join(_format_name(surnames))
+
+    return " ".join([name, surname])
 
 def main():
-    path = "./{tour}/oop.csv".format(tour=sys.argv[1])
+    path = "./bracket/{tour}/oop.csv".format(tour=sys.argv[1])
 
     df = pd.read_csv(path, header=None)
     players = []
@@ -28,7 +39,7 @@ def main():
         for player in match.split(' {spl} '.format(spl=sys.argv[2])):
             players.append(format_name(player))
     
-    out_path = "./{tour}/tip.txt".format(tour=sys.argv[1])
+    out_path = "./bracket/{tour}/oop.txt".format(tour=sys.argv[1])
     with open(out_path, mode='w') as csv_file:
         for player in players:
             csv_file.write("{player}\n".format(player=player))
